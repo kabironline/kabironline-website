2be324ebe11e35bb96d0c42a411aca62490bedf5
Kabir Maniar <kabir.maniar@maniartech.com>
2024-06-10 17:05:59 +0530

    Project Setup and top navbar

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={`/`}>
+          <h1 className="text-4xl font-semibold">
+            Kabir<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href={`/contact`}>
+            <Button className="">Hire-me</Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">Mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
