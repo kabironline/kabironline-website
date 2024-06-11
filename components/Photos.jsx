42d9790927f7b106bcccf67b10db440f91fa9964
Kabir Maniar <kabir.maniar@maniartech.com>
2024-06-11 14:32:48 +0530

    Completed Resume Page

--- components/Photos.jsx
+++ components/Photos.jsx
@@ -31,33 +31,40 @@ const Photos = () => {
           />
         </motion.div>
       </motion.div>
-
-      <motion.svg
-        className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-        fill="transparent"
-        viewBox="0 0 506 506"
-        xmlns="http://www.w3.org/2000/svg"
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
       >
-        <motion.circle
-          cx="253"
-          cy="253"
-          r="250"
-          stroke="#00ff99"
-          strokeWidth={4}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          initial={{ strokeDasharray: "24 10 0 0" }}
-          animate={{
-            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-            rotate: [120, 60],
-          }}
-          transition={{
-            duration: 20,
-            repeat: Infinity,
-            repeatType: "reverse",
-          }}
-        />
-      </motion.svg>
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth={4}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 60],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </motion.div>
     </div>
   );
 };
