48019d2d6a4cb52bcdb01ba0d69f516d6a1693d4
Kabir Maniar <kabir.maniar@maniartech.com>
2024-09-26 12:15:51 +0530

    Added a slight scaling animation in the glass class

--- components/ResumeTabs.jsx
+++ components/ResumeTabs.jsx
@@ -11,10 +11,7 @@ import { about, education, experience, skills } from "@/lib/constants";
 
 const ResumeTabs = () => {
   return (
-    <Tabs
-      defaultValue="about"
-      className="flex flex-col xl:flex-row gap-[60px]"
-    >
+    <Tabs defaultValue="about" className="flex flex-col xl:flex-row gap-[60px]">
       <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
         <TabsTrigger value="about">About Me</TabsTrigger>
         <TabsTrigger value="experience">Experience</TabsTrigger>
@@ -33,7 +30,7 @@ const ResumeTabs = () => {
                 {experience.items.map((item, index) => (
                   <li
                     key={index}
-                    className="glass h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                    className="glass h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-2"
                   >
                     <span className="text-accent">{item.duration}</span>
                     <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
@@ -50,7 +47,7 @@ const ResumeTabs = () => {
           </div>
         </TabsContent>
         <TabsContent value="education" className="w-full">
-          <div className="flex flex-col gap-[30px] text-center xl:text-left">
+          <div className="flex flex-col gap-[20px] p-2 text-center xl:text-left">
             <h3 className="text-4xl font-bold">{education.title}</h3>
             <p className="text-white/60 mx-auto xl:mx-0">
               {education.description}
@@ -105,9 +102,7 @@ const ResumeTabs = () => {
         <TabsContent value="about" className="w-full">
           <div className="flex flex-col gap-[30px] text-center xl:text-left">
             <h3 className="text-4xl font-bold">{about.title}</h3>
-            <p className="text-white/60 mx-auto xl:mx-0">
-              {about.description}
-            </p>
+            <p className="text-white/60 mx-auto xl:mx-0">{about.description}</p>
             <ScrollArea className="h-[400px]">
               <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] gap-y-6 max-w-[620px] mx-auto xl:mx-0">
                 {about.info.map((item, index) => (
